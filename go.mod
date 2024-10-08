45728b21c8a3005058b3bcb0dbff744f04ae66c6
Neo shen <s2252215170@gmail.com>
2024-10-08 01:46:07 -0400

    fixed some error about starting

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/shenzhuangzhi1/Order-Collector
 
 go 1.23.2
 
-require github.com/gorilla/websocket v1.5.3
+require (
+	github.com/gorilla/websocket v1.5.3
+	github.com/lib/pq v1.10.9
+)
--- postgres.sql
+++ postgres.sql
@@ -4,7 +4,8 @@ drop table if exists oxk_pepe_spot;
 create table if not exists oxk_pepe_spot
 (
     id uuid primary key default gen_random_uuid(),
-    created_at timestamp with time zone default now(),
+    message text,
+    created_at timestamp with time zone default now()
 
 );
 
