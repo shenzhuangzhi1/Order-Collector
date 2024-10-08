f86892eaed8da68478abc465fedc314d2ef27b0c
lingyunmo <2087789639@qq.com>
2024-10-08 16:17:58 +0800

    Update database and revise main.go

--- postgres.sql
+++ postgres.sql
@@ -1,11 +1,11 @@
 CREATE EXTENSION IF NOT EXISTS "pgcrypto";
 
 drop table if exists oxk_pepe_spot;
+
 create table if not exists oxk_pepe_spot
 (
-    id uuid primary key default gen_random_uuid(),
-    message text,
-    created_at timestamp with time zone default now()
-
+    id         uuid primary key         default gen_random_uuid(),
+    message    text,
+    created_at timestamp with time zone default now(),
+    timestamp  text
 );
-
